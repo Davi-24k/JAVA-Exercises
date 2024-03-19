cf9c54edde863302c97245a23877b93dc886e771
Davi Manoel <davijm_00@outlook.com>
2024-03-19 11:13:17 -0300

    Update

--- src/lista_04_Estrutura_Repetitiva_FOR/Exe_01.java
+++ src/lista_04_Estrutura_Repetitiva_FOR/Exe_01.java
@@ -1,5 +1,21 @@
 package lista_04_Estrutura_Repetitiva_FOR;
 
+import java.util.Scanner;
+
 public class Exe_01 {
 
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		
+		for (int i=1; i<=x; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		
+		sc.close();
+	}
 }
--- src/lista_04_Estrutura_Repetitiva_FOR/Exe_07.java
+++ src/lista_04_Estrutura_Repetitiva_FOR/Exe_07.java
@@ -1,5 +1,23 @@
 package lista_04_Estrutura_Repetitiva_FOR;
 
+import java.util.Scanner;
+
 public class Exe_07 {
 
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+
+			int primeiro = i;
+			int segundo = i * i;
+			int terceiro = i * i * i;
+			System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+		}
+		
+		sc.close();
+	}
 }
